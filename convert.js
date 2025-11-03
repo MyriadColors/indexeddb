4a2c2800ad6cb36b8b692cc755f07eaba467e5f0
MyriadColors <pstbarbosa@gmail.com>
2025-11-03 08:10:13 -0300

    Refactor `key` function to use ES6 module syntax and improve error handling. Update error messages for invalid keys, including NaN values, invalid dates, sparse arrays, circular references, and unsupported object types.

--- convert.js
+++ convert.js
@@ -1,10 +1,10 @@
 const DOMException = require('domexception/lib/DOMException')
 
-exports.key = function (globalObject, value, path = []) {
+export function key (globalObject, value, path = []) {
     switch (typeof value) {
     case 'number': {
-            if (isNaN(value)) {
-                throw DOMException.create(globalObject, [ 'TODO: message', 'DataError' ], {})
+            if (Number.isNaN(value)) {
+                throw DOMException.create(globalObject, [ 'Invalid key: NaN values are not allowed as IndexedDB keys', 'DataError' ], {})
             }
             return value
         }
@@ -13,27 +13,27 @@ exports.key = function (globalObject, value, path = []) {
         }
     case 'object': {
             if (value instanceof Date) {
-                if (isNaN(value.getTime())) {
-                    throw DOMException.create(globalObject, [ 'TODO: message', 'DataError' ], {})
+                if (Number.isNaN(value.getTime())) {
+                    throw DOMException.create(globalObject, [ 'Invalid key: Invalid date values (NaN) are not allowed as IndexedDB keys', 'DataError' ], {})
                 }
                 return value
             } else if (Array.isArray(value)) {
                 const keys = [], subPath = path.concat(value)
                 for (let i = 0, I = value.length; i < I; i++) {
-                    if (! value.hasOwnProperty(i)) {
-                        throw DOMException.create(globalObject, [ 'TODO: message', 'DataError' ], {})
+                    if (! Object.hasOwn(value, i)) {
+                        throw DOMException.create(globalObject, [ 'Invalid key: Sparse arrays are not allowed as IndexedDB keys (all array indices must be present)', 'DataError' ], {})
                     }
                     if (~path.indexOf(value[i])) {
-                        throw DOMException.create(globalObject, [ 'TODO: message', 'DataError' ], {})
+                        throw DOMException.create(globalObject, [ 'Invalid key: Circular references are not allowed in IndexedDB key arrays', 'DataError' ], {})
                     }
-                    keys.push(exports.key(globalObject, value[i], subPath))
+                    keys.push(key(globalObject, value[i], subPath))
                 }
                 return keys
             }
-            throw DOMException.create(globalObject, [ 'TODO: message', 'DataError' ], {})
+            throw DOMException.create(globalObject, [ 'Invalid key: Object type is not supported as IndexedDB key (only number, string, Date, and arrays of these types are allowed)', 'DataError' ], {})
         }
     default: {
-            throw DOMException.create(globalObject, [ 'TODO: message', 'DataError' ], {})
+            throw DOMException.create(globalObject, [ 'Invalid key: Type is not supported as IndexedDB key', 'DataError' ], {})
         }
     }
 }
