3886208214e6365185105f9b8663f80d83cd376e
MyriadColors <pstbarbosa@gmail.com>
2025-11-04 00:39:56 -0300

    Refactor idbcursor_advance_index test to use arrow functions and named function expressions for event handlers. Update variable declarations to use let/const for improved clarity and maintainability. Adjust record object property order for consistency.

--- test/idbcursor_advance_index.wpt.t.js
+++ test/idbcursor_advance_index.wpt.t.js
@@ -1,49 +1,52 @@
 require('proof')(4, async okay => {
     await require('./harness')(okay, 'idbcursor_advance_index')
-    await harness(async function () {
+    await harness(async () => {
         var db,
           count = 0,
           t = async_test(),
-          records = [ { pKey: "primaryKey_0", iKey: "indexKey_0" },
-                      { pKey: "primaryKey_1", iKey: "indexKey_1" },
-                      { pKey: "primaryKey_2", iKey: "indexKey_2" },
-                      { pKey: "primaryKey_3", iKey: "indexKey_3" }];
+          records = [ { iKey: "indexKey_0", pKey: "primaryKey_0" },
+                      { iKey: "indexKey_1", pKey: "primaryKey_1" },
+                      { iKey: "indexKey_2", pKey: "primaryKey_2" },
+                      { iKey: "indexKey_3", pKey: "primaryKey_3" }];
 
         var open_rq = createdb(t);
-        open_rq.onupgradeneeded = function(e) {
+        open_rq.onupgradeneeded = function onupgradeneeded(e) {
             db = e.target.result;
             var store = db.createObjectStore("test", {keyPath:"pKey"});
             store.createIndex("idx", "iKey");
 
-            for(var i = 0; i < records.length; i++) {
+            for(let i = 0; i < records.length; i++) {
                 store.add(records[i]);
             }
         };
 
-        open_rq.onsuccess = function (e) {
+        open_rq.onsuccess = function  onsuccess(e) {
             var cursor_rq = db.transaction("test")
                               .objectStore("test")
                               .index("idx")
                               .openCursor();
 
-            cursor_rq.onsuccess = t.step_func(function(e) {
+            cursor_rq.onsuccess = t.step_func(function onsuccess(e) {
                 var cursor = e.target.result;
                 assert_true(cursor instanceof IDBCursor);
 
                 switch(count) {
-                    case 0:
+                    case 0: {
                         count += 3;
                         cursor.advance(3);
                         break;
-                    case 3:
-                        var record = cursor.value;
+                    }
+                    case 3: {
+                        const record = cursor.value;
                         assert_equals(record.pKey, records[count].pKey, "record.pKey");
                         assert_equals(record.iKey, records[count].iKey, "record.iKey");
                         t.done();
                         break;
-                    default:
+                    }
+                    default: {
                         assert_unreached("unexpected count");
                         break;
+                    }
                 }
             });
         }
